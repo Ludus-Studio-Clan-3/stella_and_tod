9f3973ab3e56e2b05041da6119620f98136d18a3
Mirana RAKOTONIRINA <mihrakotonirina@gmail.com>
2025-12-01 16:28:34 +0300

    fix player mode switch in Player_mode_chager.gd

--- scenes/quests/story_quests/stella_and_the_threa/1_Line tracing/player_mode_changer.gd
+++ scenes/quests/story_quests/stella_and_the_threa/1_Line tracing/player_mode_changer.gd
@@ -4,3 +4,14 @@ extends Area2D
 
 func _ready() -> void:
 	pass
+
+		 
+func _on_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("player")):
+		player.change_mode_from_number(1)
+
+
+func _on_body_exited(body: Node2D) -> void:
+
+	if(body.is_in_group("player")):
+		player.change_mode_from_number(3)
