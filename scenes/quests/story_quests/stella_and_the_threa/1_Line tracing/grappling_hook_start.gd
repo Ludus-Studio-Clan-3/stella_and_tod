4ab038b55c971910076f70ba391b4407f9ac9f02
Mirana RAKOTONIRINA <mihrakotonirina@gmail.com>
2025-12-01 22:01:31 +0300

    add fog into line_tracing quest

---
+++ scenes/quests/story_quests/stella_and_the_threa/1_Line tracing/grappling_hook_start.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+@onready var fog_mat = $Fog/ParallaxLayer/ColorRect.material
+
+var fog_density = 0.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	fog_density = clamp(fog_density + delta * 0.02 , 0.0, 5.0)
+	fog_mat.set_shader_parameter("density", fog_density)   
